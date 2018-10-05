f4b6ef45814da3f4974fec5f245991c24533e224
hugang <frontwebvueapp@163.com>
2018-10-05 19:01:42 +0800

    框架修改

--- studentsys.cpp
+++ studentsys.cpp
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+// 定义学生有哪些属性
+struct Student {
+   int  id; // 学号
+   char SName[20]; // 姓名
+   float Score;  // 分数
+};
+// 定义70人的数组
+Student stu[70];
+
 /*
   菜单选择，
   输入对应的数字，进入到对应的功能
 */
-
 char menu() {
 	char c;
 	printf("\t\t ╭═════════■□■□═══╮\n");  
@@ -22,10 +30,11 @@ char menu() {
 	c = getchar();
 	return c;
 }
-// 比如功能封装函数
-void addStudent(){
-	system("cls");
-    printf("欢迎来到添加学生信息功能\n");
+// 添加学生信息，可以一次添加n条
+void addStudent(Student *sp){
+	printf("\t\t添加数据: ");
+	scanf("%d%s%f", &sp->id, sp->SName, &sp->Score);
+	printf("\t\t%d %s %.2f", sp->id, sp->SName, sp->Score);
 }
 
 // 退出功能
@@ -35,27 +44,20 @@ void exitSystem(){
 }
 
 // 功能项,如果有其他的项再继续添加
-void initFeture(char select){
+void initFeture(char select, Student *sp){
 	switch(select){
 	  case '0':
 		  exitSystem();
 		  break;
 	  case '1':
-		  addStudent();
+		  addStudent(sp);
 		  break;
 	}
 }
 
 void main () {
-	// 定义学生有哪些属性
-	struct Student {
-	   int  id; // 学号
-       char SName[20]; // 姓名
-	   float Score;  // 分数
-	};
-	// 定义70人的数组
-	Student stu[70];
+	// 调用菜单选择功能项
 	char selectMenu = menu();
 	// 初始化功能入口
-	initFeture(selectMenu);
+	initFeture(selectMenu, stu);
 }
